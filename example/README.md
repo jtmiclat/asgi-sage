1c4c52c45887aae1ff1ac51cf29b55c0ef6358e1
Jt Miclat <jtmiclat@gmail.com>
2020-05-26 03:58:40 +0800

    Deploy example app

---
+++ example/README.md
@@ -0,0 +1,5 @@
+# Asgi Sage Example
+Deployed to: https://asgi-sage-example.jtmiclat.me/
+
+## Grade the appliacation
+https://securityheaders.com/?q=https%3A%2F%2Fasgi-sage-example.jtmiclat.me%2F&followRedirects=on
---
+++ example/app.py
@@ -0,0 +1,19 @@
+from starlette.applications import Starlette
+from starlette.responses import PlainTextResponse
+from asgi_sage.middleware import SageMiddleware
+
+app = Starlette()
+app.add_middleware(
+    SageMiddleware,
+    feature_policy={"geolocation": "*", "usb": "'self'"},
+    content_security_policy={"default-src": "'self"},
+)
+
+
+@app.route("/")
+async def index(request):
+
+    response = PlainTextResponse("ok")
+    response.set_cookie("key", "value")
+    response.set_cookie("key2", "value2")
+    return response
